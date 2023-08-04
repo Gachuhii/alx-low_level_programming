003e07795e3a202ed0069a083f9b68bfcd88abb4
gachuhii <you@gachuhidavid03@gmail.com>
2023-08-04 01:41:37 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _sqrt_helper - Helper function to recursively find the square root.
@@ -10,21 +10,21 @@
  */
 int _sqrt_helper(int n, int start, int end)
 {
-    int mid;
-    int square;
+	int mid;
+	int square;
 
-    if (start > end)
-        return (-1);
+	if (start > end)
+		return (-1);
 
-    mid = start + (end - start) / 2;
-    square = mid * mid;
+	mid = start + (end - start) / 2;
+	square = mid * mid;
 
-    if (square == n)
-        return (mid);
-    else if (square < n)
-        return (_sqrt_helper(n, mid + 1, end));
-    else
-        return (_sqrt_helper(n, start, mid - 1));
+	if (square == n)
+		return (mid);
+	else if (square < n)
+		return (_sqrt_helper(n, mid + 1, end));
+	else
+		return (_sqrt_helper(n, start, mid - 1));
 }
 
 /**
@@ -35,9 +35,9 @@ int _sqrt_helper(int n, int start, int end)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
+	if (n < 0)
+		return (-1);
 
-    return (_sqrt_helper(n, 0, n));
+	return (_sqrt_helper(n, 0, n));
 }
 
